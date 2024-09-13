1acf4085848e00a07c5a59b975177071ef8759ca
Angelica Marker <am225cs@student.lnu.se>
2024-09-13 11:28:57 +0200

    workshop 1 done

---
+++ app.js
@@ -0,0 +1,34 @@
+const todoInput = document.querySelector('#todoinput')
+const todoButton = document.querySelector('#todobutton')
+const todoItemList = document.querySelector('#todo__items--list')
+
+function addTodo() {
+  const itemValue = todoInput.value.trim()
+  createElements(itemValue)
+  clearInput()
+}
+
+function createElements(itemName) {
+  const listItem = document.createElement('li')
+  listItem.textContent = itemName
+  todoItemList.appendChild(listItem)
+  const deleteButton = document.createElement('button')
+  deleteButton.classList.add('delete')
+  deleteButton.textContent = 'Radera'
+  // trycker på button ta bort li
+  listItem.appendChild(deleteButton)
+  deleteButton.addEventListener('click', () => {
+    deleteItem(listItem)
+  })
+}
+
+todoButton.addEventListener('click', addTodo)
+
+function clearInput() {
+  todoInput.value = ''
+}
+
+function deleteItem(listItem) {
+  todoItemList.removeChild(listItem)
+  console.log('delete', listItem)
+}
